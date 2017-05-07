9461080f0a3ce17a4fc37ed0e1e7ed5309079f65
JeanPiere <jeanpiere.demir@gmail.com>
2017-05-07 07:52:28 +0300

    Portfolio Updated

---
+++ Motor Drive Projects/DC Motor Analysis/MotorParameters.m
@@ -0,0 +1,10 @@
+Vin= 24 ; %volt Input Voltage
+R= 0.43; L = 0.9e-3; %ohm and mH respectively Armature resistance and Inductance
+Tload = 0.75; % Nm Load torque
+Kstif=7.5; % Nm/rad Motor Shaft stiffness
+J_m = 2118e-7;  J_l = 1800e-7; %gcm^2 motor and load inertia respectively
+B = 0.18e-3; % mNm.s/rad load side friction coefficient  
+N1= 2;N2= 1; % gearbox ratio N1:N2
+Ke= 0.08; % volts/rad/sec Back EMF constant
+Ia_rated = 5.390 ; % A Rated Armature Current for Model-3 
+Kp = 100; 
